f904a41c27e3a70a8b954c319214b67d827cf981
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-09 15:59:24 +0200

    Update HTML title and enhance structure; add initial JavaScript setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+// Inizializzo le variabili che mi serviranno del DOM
+const startButton = document.querySelector("#start-button");
+const countDown = document.querySelector("#countdown");
+const gameInstructions = document.querySelector("#instructions");
+const numberList = document.querySelector("#number-list");
+
+// Il gioco inizia quando clicco su "Inizia"
+// Mi serve un eventListener per far partire il gioco
